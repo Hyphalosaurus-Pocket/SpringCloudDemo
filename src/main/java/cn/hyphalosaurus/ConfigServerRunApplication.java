fe5c0edbbc95c3bb67747e3843879b7cebd3fc9c
hyphalosaurus <656351726@qq.com>
2020-11-27 19:15:49 +0800

    yml change

--- src/main/java/cn/hyphalosaurus/ConfigServerRunApplication.java
+++ src/main/java/cn/hyphalosaurus/ConfigServerRunApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerRunApplication {
 
 	public static void main(String[] args) {
